a82acdabe3c14edeb9e6054c37a393ec7203b1a9
Asbjorn Kjaer <akjaer@riotgames.com>
2018-02-22 11:25:49 -0800

    Add update_latest to shell script

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -6,3 +6,4 @@ PYENV=/tmp/.pyenv
 python3 -m venv ${PYENV}
 ${PYENV}/bin/pip install -U -r requirements.txt
 ${PYENV}/bin/python3 build.py build --verbose
+${PYENV}/bin/python3 build.py update_latest
